b52d143338cdbcd01552463b599629b07fed16be
James Fairbanks <james.fairbanks@gtri.gatech.edu>
2017-06-23 15:05:45 -0700

    initial datastructure

--- src/DBGraphs.jl
+++ src/DBGraphs.jl
@@ -2,4 +2,61 @@ module DBGraphs
 
 # package code goes here
 
+using LightGraphs
+using JuliaDB
+import JuliaDB: DTable
+import LightGraphs: AbstractGraph, vertices, edges, neighbors, indegree, outdegree,
+       in_neighbors, out_neighbors, nv, ne, is_directed
+
+export DBGraph
+
+abstract type AbstractDBGraph <: LightGraphs.AbstractGraph end
+
+mutable struct DBGraph{T} <: AbstractDBGraph
+  table::T
+  src::Symbol
+  dst::Symbol
+  directed::Bool
+end
+
+function DBGraph(table::DTable)
+  return DBGraph(table, :src, :dst, true)
+end
+
+is_directed(g) = g.directed
+colmax(table, column) = reduce(max,map(x->getfield(x, column), table))
+nv(g::DBGraph) = max(colmax(g.table, g.src), colmax(g.table, g.dst))
+ne(g::DBGraph) = size(g.table,1)
+
+function vertices(g::DBGraph)
+  return select(g.table, g.dst, agg=first)
+end
+
+function edges(g::DBGraph)
+  return select(g.table, [g.src,g.dst])
+end
+
+# function out_neighbors(g::DBGraph, v)
+#   return g.table[v,:]
+# end
+# function in_neighbors(g::DBGraph, v)
+#   return g.table[:,v]
+# end
+# function neighbors(g::DBGraph, v)
+#   return merge(out_neighbors(g,v), in_neighbors(g, v))
+# end
+
+function out_neighbors(g::DBGraph, v::Integer)
+  return collect(map(x->x.dst, filter(x->x[1] == v, g.table)))
+end
+function in_neighbors(g::DBGraph, v::Integer)
+  return collect(map(x->x.src, filter(x->x[2] == v, g.table)))
+end
+function neighbors(g::DBGraph, v::Integer)
+  return union(out_neighbors(g,v), in_neighbors(g, v))
+end
+
+indegree(g::DBGraph, v::Integer) = length(collect(in_neighbors(g, v)))
+outdegree(g::DBGraph, v::Integer) = length(collect(out_neighbors(g, v)))
+
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using DBGraphs
 using Base.Test
-
+using JuliaDB
+using LightGraphs
 # write your own tests here
-@test 1 == 2
+
+path = Pkg.dir("DBGraphs", "test", "data")
+# g = DBGraph(loadfiles(path, indexcols=["src","dst"]))
+g = DBGraph(loadfiles(path))
+
+@show g
+@test g.src == :src
+@test g.dst == :dst
+# @test 2 ∈ neighbors(g, 1)
+
+@test outdegree(g, 1) == 2
+@test indegree(g, 1) == 0
+@test indegree(g, 3) == 0
+@test outdegree(g, 3) == 2
+
+@test ne(bfs_tree(g, 1)) == 1
+@test ne(bfs_tree(g, 3)) == 10
+for e in edges(bfs_tree(g, 3))
+  println(e)
+end
